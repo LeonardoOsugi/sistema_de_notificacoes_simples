3d0b28b483a5977c84f5bd71c7b139cdfa86ba9e
Nicolas Faustino <69408074+nicolasfaustino@users.noreply.github.com>
2026-06-10 19:12:28 -0300

    Add notification tracking in postNotify function
    Register sent and failed notifications in the notify controller.

--- src/controllers/notifyController.js
+++ src/controllers/notifyController.js
@@ -1,5 +1,6 @@
 
 import { sendToQueue } from '../services/notifyService.js';
+import { registerSentNotification, registerFailedNotification } from '../services/statsService.js';
 
 export async function postNotify(req, res) {
     try{
@@ -11,12 +12,14 @@ export async function postNotify(req, res) {
 
 
         await sendToQueue({ to, type, message });
-
+        await registerSentNotification();
+        
         return res.status(202).json({ 
             status: "Pedido aceito",
             detail: "Sua notificação foi enfileirada e será processada em breve." 
         });
     } catch (e) {
+        await registerFailedNotification()
         res.status(500).json({ error: "Erro interno no servidor." });;
     }
 }
